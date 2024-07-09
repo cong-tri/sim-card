8a2908841aa06a1de6df56cd3358d9788ec72524
Dao Cong Tri <daocongtri20031609@gmail.com>
2024-07-09 13:59:29 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="h-[100vh] bg-blue-300">
       <div className="container flex h-full justify-center items-center">
-        <h1 className="text-3xl text-white">Welcome to Sim Card</h1>
+        <h1 className="sm:text-xl lg:text-3xl text-white">Welcome to Sim Card</h1>
       </div>
     </main>
   );
