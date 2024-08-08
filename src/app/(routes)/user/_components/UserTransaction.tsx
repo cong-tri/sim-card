aeb944c7eba34ecc68595f711618efcce3bc85b7
Dao Cong Tri <daocongtri20031609@gmail.com>
2024-08-08 12:59:35 +0700

    update

--- src/app/(routes)/user/_components/UserTransaction.tsx
+++ src/app/(routes)/user/_components/UserTransaction.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import React from "react";
+
 import { useUserContext } from "@/context/UserProvider";
+
 import { Table, Typography } from "antd";
 import type { TableProps } from "antd";
 import Title from "antd/es/typography/Title";
+
 import moment from "moment";
 
 const columns: TableProps["columns"] = [
@@ -39,8 +42,13 @@ const { Text } = Typography;
 
 export default function UserTransaction() {
   const { transaction } = useUserContext();
+
   // in order to show the latest transaction into layout
-  const reserveTransaction = [...transaction ?? []].reverse()
+  const reserveTransaction = [...(transaction ?? [])].reverse();
+  const totalAmount = reserveTransaction.reduce(
+    (total, transaction) => total + transaction.amount,
+    0
+  );
   return (
     <>
       <div className="my-8 px-14">
@@ -51,20 +59,14 @@ export default function UserTransaction() {
               key: `${index + 1}`,
               name: item.product,
               date: moment(item.date).format("MMMM Do YYYY, h:mm:ss A"),
-              amount:
-                item.amount < 0 ? (
-                  <Text type="danger">
-                    {item.amount} {item.currency}
-                  </Text>
-                ) : (
-                  <Text type="success">
-                    +{item.amount} {item.currency}
-                  </Text>
-                ),
-              status: item.read ? (
-                <Text>Read</Text>
-              ) : (
-                <Text type="danger">Unread</Text>
+              amount: (
+                <Text type={item.amount < 0 ? "danger" : "success"}>
+                  {item.amount} {item.currency}
+                </Text>
+              ),
+
+              status: (
+                <Text type={item.read ? "secondary" : "danger"}>{item.read ? "Read" : "Unread"}</Text>
               ),
               description: (
                 <>
@@ -96,7 +98,32 @@ export default function UserTransaction() {
               <p style={{ margin: 0 }}>{record.description}</p>
             ),
             rowExpandable: (record) => record.name !== "Not Expandable",
+            expandRowByClick: true,
           }}
+          size="large"
+          summary={() => (
+            <Table.Summary fixed>
+              <Table.Summary.Row>
+                <Table.Summary.Cell index={0} colSpan={2}>
+                  Note
+                </Table.Summary.Cell>
+                <Table.Summary.Cell index={1} colSpan={5}>
+                  This is a list of transactions from the previous 10 days to
+                  the current date.
+                </Table.Summary.Cell>
+              </Table.Summary.Row>
+              <Table.Summary.Row>
+                <Table.Summary.Cell index={0} colSpan={2}>
+                  Total Amount
+                </Table.Summary.Cell>
+                <Table.Summary.Cell index={1} colSpan={5}>
+                  <Text type={totalAmount < 0 ? "danger" : "success"}>
+                    {totalAmount} VND
+                  </Text>
+                </Table.Summary.Cell>
+              </Table.Summary.Row>
+            </Table.Summary>
+          )}
           title={() => {
             return <Title>Transaction</Title>;
           }}
